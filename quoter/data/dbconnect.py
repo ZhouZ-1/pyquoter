851b4f6a12361772c5425a7f5f6ce40d637b064d
joshu <joshuapetitma@yahoo.com>
2020-11-10 06:57:58 -0500

    all_quotes returns Quote objects

--- quoter/data/dbconnect.py
+++ quoter/data/dbconnect.py
@@ -15,11 +15,19 @@ create_table_command = '''CREATE TABLE IF NOT EXISTS quotes (
 command.execute(create_table_command)
 
 insert_quote_command = "INSERT INTO quotes(quote, author, date) VALUES (?, ?, ?)"
-
+query_all_command = 'SELECT * FROM quotes;'
+drop_table_command = 'DROP TABLE IF EXISTS quotes'
 
 def insert_quote(quote: Quote):
     command.execute(insert_quote_command,
                     [quote.quote, quote.author, quote.date])
 def all_quotes():
-    quotes = command.execute('SELECT * FROM quotes;')
-    return quotes
+    response = []
+    quotes = command.execute(query_all_command)
+    for quote in quotes:
+        response.append(Quote(quote[1], quote[2], quote[3], quote[0]))
+    return response
+
+def reset_db():
+    command.execute(drop_table_command)
+    command.execute(create_table_command)
