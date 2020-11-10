b02623032e2ac135e86d141e2884f98dd84d20ef
joshu <joshuapetitma@yahoo.com>
2020-11-10 07:00:58 -0500

    adds test for data.dbconnect.insert_quote

--- test/database_test.py
+++ test/database_test.py
@@ -8,9 +8,15 @@ from quoter.model import *
 class TestDataBase(unittest.TestCase):
 
     def test_insertion(self):
+        reset_db()
         quote = Quote('test_quote', 'test_author', '2000-01-01 10:00:00')
         insert_quote(quote)
         quotes = all_quotes()
+        quote = quotes[0]
+        self.assertTrue(quote.quote == 'test_quote')
+        self.assertTrue(quote.author == 'test_author')
+        self.assertTrue(quote.date == '2000-01-01 10:00:00')
+        self.assertTrue(quote.quote_id == 1)
 
 if __name__ == '__main__':
     unittest.main()
