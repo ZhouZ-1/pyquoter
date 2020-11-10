2351d959d8465f223366d5571e936df7c9ca9225
joshu <joshuapetitma@yahoo.com>
2020-11-10 06:57:32 -0500

    adds quote_id field to Quote object

--- quoter/model/quote.py
+++ quoter/model/quote.py
@@ -1,7 +1,8 @@
 class Quote:
-    def __init__(self, quote: str, author='', date='', tags=[]):
+    def __init__(self, quote: str, author='', date='', quote_id = None, tags=[]):
         self.quote = quote
         self.author = author
         self.date = date
         self.tags = tags
+        self.quote_id = quote_id
 
